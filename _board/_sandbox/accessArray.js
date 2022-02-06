d937931c45c1f790f32f9afd711292f53ed20a65
Jevarom <ni.moravej@gmail.com>
2022-02-06 15:49:16 -0800

    Create folders testbench and sandbox
    New folders for organization. Created testbench script for code and sandbox for trial and error of board creation.

---
+++ _board/_sandbox/accessArray.js
@@ -0,0 +1,8 @@
+let arr = ['This is the first element', 'This is the second element', 'This is the last element']
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[arr.length - 1])
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
