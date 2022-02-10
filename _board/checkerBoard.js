793424a02003dff2f6f61d18f4314465ffcbed89
halycon-days <dhking@usc.edu>
2022-02-09 21:54:40 -0800

    added setters to checkerPiece, tweaked 2d array mechanics in checkerboard, added concepts for new methods

--- _board/checkerBoard.js
+++ _board/checkerBoard.js
@@ -6,20 +6,82 @@ const BLACK = 5;
 
 class CheckerBoard {
 
+    /*The checkerboard is represented by a two dimensional array.
+    Undefined indicates that a space is empty, spaces with a Checker object
+    indicate that there is a checker occupying that space.
+    
+    The checkerboard also keeps track of remaining pieces on the board.*/ 
     constructor(){
-        let red_Pieces = 16;
-        let black_Pieces = 16;
-        let color_flag = FALSE;
+        let red_Pieces = 0;
+        let black_Pieces = 0;
         let arr = new Array(8); // create an empty array of length n
         for (var i = 0; i < 8; i++) {
-            if (color_flag == FALSE){
-                arr[i] = new Array(WHITE, BLACK, WHITE, BLACK, WHITE, BLACK, WHITE, BLACK); 
-                color_flag = TRUE;
+            arr[i] = new Array(8); 
+        }
+    }
+
+    /*Initializes the board state to a new game.
+    The middle two rows of the checkerboard are always initialized as empty.*/
+    set_board(){
+        //let odd_flag = FALSE;
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                if (i == 3 || i == 4){
+                    continue;
+                }
+                else if (i < 3){
+                    if (i%2==0){
+                        //is this legal syntax?
+                        let newCheck = new Checker();
+                        newCheck.setPos(i,j);
+                        newCheck.setState(NORMAL_STATE);
+                        //newCheck.setPlayer();
+
+                        //this.arr[i][j]==
+                    }
+
+                }
             }
-            else {
-                arr[i] = new Array(BLACK, WHITE, BLACK, WHITE, BLACK, WHITE, BLACK, WHITE); 
-                color_flag = FALSE;
-            }
+
+        }
+
+    }
+
+    move_piece(Checker,horiz_pos, vert_pos){
+        if(is_valid_move(horiz_pos, vert_pos)==TRUE){
+
+        }
+    }
+
+    swap_player(){
+
+    }
+
+    //potentially useful if we want to rotate the board for perspective purposes
+    reverse_board(){
+
+    }
+
+    capture_piece(Checker){
+        
+    }
+
+    //this function checks to see if a certain latitude and longitude is outside the bounds of the board
+    static check_edges(horiz_pos, vert_pos){
+        if (horiz_pos > 7 || horiz_pos < 0){
+            return FALSE;
+        }
+        
+        if (vert_pos > 7 || vert_pos < 0){
+            return FALSE; 
+        }
+
+        return TRUE;
+    }
+
+    static is_valid_move(horiz_pos, vert_pos){
+        if (check_edges(horiz_pos, vert_pos)==FALSE){
+            return FALSE;
         }
     }
 
--- _board/checkerPiece.js
+++ _board/checkerPiece.js
@@ -3,29 +3,45 @@ const INVALID_STATE = -1;
 const REMOVED_STATE = 0;
 const NORMAL_STATE = 1;
 const KING_STATE = 2;
+const PLAYER_1 = 7;
+const PLAYER_2 = 8;
 
 class Checker {
     constructor(){
         let horiz_pos = INVALID_STATE;
         let vert_pos = INVALID_STATE;
         let state = INVALID_STATE;
+        let player = INVALID_STATE;
         //let color = ...? must be some js library for color?
     }
 
-    getLatitude(Checker){
+    getLatitude(){
         return horiz_pos;
     }
 
-    getLongitude(Checker){
+    getLongitude(){
         return vert_pos;
     }
 
-    getState(Checker){
+    getState(){
         return state;
     }
 
-    
+    getPlayer(){
+        return player;
+    }
 
+    setState(new_state){
+        this.state = new_state;
+    }
 
+    setPos(new_horiz, new_vert){
+        this.horiz_pos = new_horiz;
+        this.vert_pos = new_vert;
+    }
+
+    setPlayer(new_player){
+        this.player = new_player;
+    }
 
 }
