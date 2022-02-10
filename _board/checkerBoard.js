316c2f105be56fd2eafd72e6d7ff27bfe5003f52
halycon-days <dhking@usc.edu>
2022-02-10 00:08:45 -0800

    why study for my algorithms exam when i can tweak checkers

--- _board/checkerBoard.js
+++ _board/checkerBoard.js
@@ -1,9 +1,6 @@
 
 /*import `./checkerPiece.js`;*/ //doesn't seem to work
 
-const WHITE = 4;
-const BLACK = 5;
-
 class CheckerBoard {
 
     /*The checkerboard is represented by a two dimensional array.
@@ -21,43 +18,64 @@ class CheckerBoard {
     }
 
     /*Initializes the board state to a new game.
-    The middle two rows of the checkerboard are always initialized as empty.*/
+    The middle two rows of the checkerboard are always initialized as empty.
+    
+    Method looks kind of ugly rn. If any ideas to refactor, go for it*/
     set_board(){
-        //let odd_flag = FALSE;
+
         for (var i = 0; i < 8; i++) {
             for (var j = 0; j < 8; j++) {
                 if (i == 3 || i == 4){
                     continue;
                 }
-                else if (i < 3){
+                else {
+                    let newCheck = new Checker();
                     if (i%2==0){
-                        //is this legal syntax?
-                        let newCheck = new Checker();
-                        newCheck.setPos(i,j);
-                        newCheck.setState(NORMAL_STATE);
-                        //newCheck.setPlayer();
-
-                        //this.arr[i][j]==
+                        if (j%2==0){
+                            newCheck.setPos(i,j);
+                            newCheck.setState(NORMAL_STATE);
+                            //newCheck.setPlayer();
+                            this.arr[i][j]== newCheck;
+                        }   
+                    }
+                    else {
+                        if (j%2!=0){
+                            newCheck.setPos(i,j);
+                            newCheck.setState(NORMAL_STATE);
+                            //newCheck.setPlayer();
+                            this.arr[i][j]== newCheck;
+                        }   
                     }
 
                 }
             }
 
         }
+        this.red_Pieces = 12;
+        this.black_Pieces = 12;
 
     }
 
-    move_piece(Checker,horiz_pos, vert_pos){
+    /*Used to move a piece to a certain square. Calls helper methods to perform
+    legal move checking. Needs to set the index that it came from to undefined.
+    I want to say that the actual movement is governed by the GUI...
+        @param big_checker - Checker object that is being moved
+        @param horiz_pos - space we are trying to move to (latitude)
+        @param vert_pos - space we are trying to move to (longitude)
+    */
+    move_piece(big_checker,horiz_pos, vert_pos){
         if(is_valid_move(horiz_pos, vert_pos)==TRUE){
 
         }
+        this.arr[big_checker.getLatitude][big_checker.getLongitude]=undefined;
+        big_checker.setPos(horiz_pos, vert_pos);
     }
 
     swap_player(){
 
     }
 
-    //potentially useful if we want to rotate the board for perspective purposes
+    //potentially useful if we want to rotate the board for perspective purposes. 
     reverse_board(){
 
     }
@@ -66,7 +84,10 @@ class CheckerBoard {
         
     }
 
-    //this function checks to see if a certain latitude and longitude is outside the bounds of the board
+    /*Checks corner case to ensure move is not of the board.
+        @param horiz_pos - space we are trying to move to (latitude)
+        @param vert_pos - space we are trying to move to (longitude)
+    */
     static check_edges(horiz_pos, vert_pos){
         if (horiz_pos > 7 || horiz_pos < 0){
             return FALSE;
@@ -79,10 +100,33 @@ class CheckerBoard {
         return TRUE;
     }
 
-    static is_valid_move(horiz_pos, vert_pos){
+    /*This function checks to see if a move is valid. Uses check_edges and additionally
+    detects if the space where the checker is attempting to move is occupied by a checker. 
+        @param big_checker - Checker object that is being moved
+        @param horiz_pos - space we are trying to move to (latitude)
+        @param vert_pos - space we are trying to move to (longitude)
+    */
+    static is_valid_move(big_checker, horiz_pos, vert_pos){
         if (check_edges(horiz_pos, vert_pos)==FALSE){
             return FALSE;
         }
+        if(this.arr[horiz_pos][vert_pos]!=undefined){
+            return FALSE;
+        }
+        return TRUE;
+
+    }
+
+    static is_valid_capture(big_checker, horiz_pos, vert_pos){
+        if (check_edges(horiz_pos, vert_pos)==FALSE){
+            return FALSE;
+        }
+        if(this.arr[horiz_pos][vert_pos]!=undefined){
+            return FALSE;
+        }
+        //add error checking to make sure there's a checker being hopped over
+        return TRUE;
+
     }
 
 }
