3e69881d092e6d6773c86ce96cbaa5e1e619d068
Jevarom <ni.moravej@gmail.com>
2022-02-11 18:52:33 -0800

    Organize folder, files

--- _board/_sandbox/accessArray.js
+++ _board/_sandbox/accessArray.js
@@ -3,6 +3,26 @@ console.log(arr[0])
 console.log(arr[1])
 console.log(arr[arr.length - 1])
 
+console.log("\n")
+
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i])
 }
+
+console.log("\n")
+
+for (let i in arr) {
+    console.log(arr[i])
+}
+
+console.log("\n")
+
+for (let x of arr) {
+    console.log(arr[x])
+}
+
+//Why does "for of" loop not working?
+
+let fruits = ['Apple', 'Banana']
+console.log(fruits.length)
+fruits.forEach(function(item,index,array){console.log(item,index)})
